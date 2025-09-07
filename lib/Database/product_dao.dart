ac1acf2cfc049e630aafb4e49776184ed9488daf
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-09-07 20:00:33 +0500

    For hdanling products

---
+++ lib/Database/product_dao.dart
@@ -0,0 +1,22 @@
+import 'package:sqflite/sqflite.dart';
+import '../Model/product.dart';
+
+class ProductDao {
+  final Database db;
+  ProductDao(this.db);
+
+  Future<int> insertProduct(Product product) async {
+    return await db.insert('Products', product.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<List<Product>> getAllProducts() async {
+    final result = await db.query('Products', orderBy: 'name ASC');
+    return result.map((map) => Product.fromMap(map)).toList();
+  }
+
+  Future<List<String>> getDistinctCategories() async {
+    final result = await db.rawQuery("SELECT DISTINCT category FROM Products");
+    return result.map((row) => row['category'] as String).toList();
+  }
+}
